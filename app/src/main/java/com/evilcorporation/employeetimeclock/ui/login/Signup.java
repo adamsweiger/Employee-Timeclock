961876067081eb9cf009f534a33bebcbe54d8d6b
Meet Patel <mjpatel@iastate.edu>
2020-02-05 10:02:39 -0600

    Added total number of hours to count for an employee

--- app/src/main/java/com/evilcorporation/employeetimeclock/ui/login/Signup.java
+++ app/src/main/java/com/evilcorporation/employeetimeclock/ui/login/Signup.java
@@ -1,8 +1,31 @@
 package com.evilcorporation.employeetimeclock.ui.login;
+import androidx.annotation.Nullable;
 
 public class Signup {
-    public static void main(String[] args){
-        Scanner input = 
+    private int DisplayName;
+    private int count;
+    private int hours;
+    private int time;
+
+    public Signup(int givenDisplayName, int givencount, int givenhours, int giventime){
+        DisplayName = givenDisplayName;
+        count = givencount;
+        hours = int givenhours;
+        time = int giventime;
+
     }
+    public int getHours() {
+        return hours + time
+    }
+    public int getDisplayName() {
+        return DisplayName;
+    }
+    public int getCount(){
+        count time + hours;
+        count++;
+    }
+
+
+
 
 }
