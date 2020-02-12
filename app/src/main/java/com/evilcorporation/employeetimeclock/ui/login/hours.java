aa9202a3306d092524689b2fb2852cd5a99c19ae
Meet Patel <mjpatel@iastate.edu>
2020-02-12 09:58:46 -0600

    Added current time and date

--- app/src/main/java/com/evilcorporation/employeetimeclock/ui/login/hours.java
+++ app/src/main/java/com/evilcorporation/employeetimeclock/ui/login/hours.java
@@ -1,9 +1,29 @@
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
-public class CurrentDateTimeExample1 {
-    public static void main(String[] args) {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        LocalDateTime now = LocalDateTime.now();
-        System.out.println(dtf.format(now));
+import com.evilcorporation.employeetimeclock.R;
+import com.evilcorporation.employeetimeclock.ui.login.LoginViewModel;
+import com.evilcorporation.employeetimeclock.ui.login.LoginViewModelFactory;
+
+public class hours {
+    private int hour;
+    private int minute;
+    private int second;
+    public hours (int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
     }
+    public Time(long time) {
+        LocalDateTime now = LocalDateTime.now();
+
+
+    }
+    public int getDate() {
+        LocalDateTime now = LocalDateTime.now();
+    }
+    //public static void main(String[] args) {
+        //DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+       // LocalDateTime now = LocalDateTime.now();
+        //System.out.println(dtf.format(now));
+
 }
