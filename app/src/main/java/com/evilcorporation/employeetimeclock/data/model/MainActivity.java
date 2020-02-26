cc0b0722222148cf2f0de2e42bc7d780adf1fb55
Adam Sweiger <asweiger@iastate.edu>
2020-02-26 00:19:44 -0600

    Added barcode scanning functionality

--- app/src/main/java/com/evilcorporation/employeetimeclock/data/model/MainActivity.java
+++ app/src/main/java/com/evilcorporation/employeetimeclock/data/model/MainActivity.java
@@ -1,6 +1,9 @@
 package com.evilcorporation.employeetimeclock.data.model;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
 import android.util.Log;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -10,6 +13,13 @@ import android.os.Bundle;
 import android.*;
 
 import com.evilcorporation.employeetimeclock.R;
+import com.google.android.gms.samples.vision.barcodereader.BarcodeCapture;
+import com.google.android.gms.samples.vision.barcodereader.BarcodeGraphic;
+import com.google.android.gms.vision.barcode.Barcode;
+
+import java.util.List;
+
+import xyz.belvi.mobilevisionbarcodescanner.BarcodeRetriever;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -24,7 +34,36 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "EmployeeHours", Toast.LENGTH_SHORT).show();
 
          */
+        BarcodeCapture barcodeCapture = (BarcodeCapture)getSupportFragmentManager().findFragmentById(R.id.barcode);
+        barcodeCapture.setRetrieval((BarcodeRetriever) this);
 
+    }
+
+    public void onPermissionRequestDenied() {
+
+    }
+
+    public void onRetrievedFailed(String reason) {
+
+    }
+
+    public void onRetrieved(final Barcode barcode) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Code Retrieved")
+                        .setMessage(barcode.displayValue);
+                builder.show();
+            }
+        });
+    }
+
+    public void onRetrievedMultiple(Barcode closetToClick, List<BarcodeGraphic> barcode) {
+
+    }
+
+    public void onBitmapScanned(SparseArray<Barcode> sparseArray) {
 
     }
 
