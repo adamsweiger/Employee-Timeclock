f2f67930b9a1687060449c48587d6f40c538d90f
Adam Sweiger <asweiger@iastate.edu>
2020-04-08 11:31:02 -0500

    Setting up Room database

---
+++ app/src/main/java/com/evilcorporation/employeetimeclock/data/model/User.java
@@ -0,0 +1,17 @@
+package com.evilcorporation.employeetimeclock.data.model;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class User {
+    @PrimaryKey
+    public int userId;
+
+    @ColumnInfo(name = "first_name")
+    public String firstName;
+
+    @ColumnInfo(name = "last_name")
+    public String lastName;
+}
